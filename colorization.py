fixing analysis calculation

colorization.py
@@ -143,12 +143,12 @@ def nearest_neighbor(tr, ts, n, pixel_dict, num_colors):
 
 # recolors the given image with the representative colors, using the color
 # specified in dict for each pixel
-def recolor(img, rep_colors, dict):
+def recolor(img, rep_colors, d):
     # iterate through the pixels and recolor based on rep color
     temp = [[0 for i in range(len(img[0]))] for j in range(len(img))]
     for r in range(len(img)):
         for c in range(len(img[0])):
-            color = rep_colors[dict[(r,c)]]
+            color = rep_colors[d[(r,c)]]
             temp[r][c] = np.array(color)
     return np.array(temp)
 
@@ -197,7 +197,8 @@ def basic_agent(img, k, n):
 if __name__ == '__main__':
     # loads image as a pixel array
     pic_path = input("Enter the path to the picture: ")
-    img = image.imread(pic_path)
+    og_img = image.imread(pic_path)
+    img = og_img.copy()
     cmd = input("Enter a letter corresponding to a command:\n(B)asic Agent\n(I)mproved Agent\n(C)ompare both\n(E)xit\n")
 
     while cmd is not "E":
@@ -210,7 +211,7 @@ if __name__ == '__main__':
                     print("n = " + str(i))
                     b_img = basic_agent(img, k, i)
                     # compare with just the right half
-                    _, right_img = np.hsplit(img, 2)
+                    _, right_img = np.hsplit(og_img, 2)
                     _, right_new = np.hsplit(b_img, 2)
                     print("Mean square error: " + str(an.pix_diff(right_img, right_new)))
             else:
@@ -221,7 +222,7 @@ if __name__ == '__main__':
                 end = time.perf_counter()
                 print(f"Elapsed Time: {end - start:0.2f} seconds")
                 # compare with just the right half
-                _, right_img = np.hsplit(img, 2)
+                _, right_img = np.hsplit(og_img, 2)
                 _, right_new = np.hsplit(b_img, 2)
                 print("Mean square error: " + str(an.pix_diff(right_img, right_new)))
             pyplot.show()
@@ -232,7 +233,8 @@ if __name__ == '__main__':
             i_img = improved_agent(img, a, e)
             end = time.perf_counter()
             print(f"Elapsed Time: {end - start:0.2f} seconds")
-            print("Mean square error: " + str(an.pix_diff(img, i_img)))
+            _, right_img = np.hsplit(og_img, 2)
+            print("Mean square error: " + str(an.pix_diff(right_img, i_img)))
             pyplot.show()
 
         cmd = input("\nEnter a letter corresponding to a command:\n(B)asic Agent\n(I)mproved Agent\n(C)ompare both\n(E)xit\n")

improved.py
@@ -56,7 +56,8 @@ def sigmoid(w, x):
     # dot sum the vectors
     # first add the bias term
     sum = w[-1]
-    sum += np.dot(w[:-1],x)
+    for i in range(len(x)):
+        sum += (w[i]*x[i])
     # compute the sigmoid value
     sig = 1.0 + math.exp(-sum)
     return (1.0/sig)
@@ -151,8 +152,9 @@ def improved_agent(img, alpha, epoch):
     # input: 1x9 vector
     # output: 1x3 vector
     nn = [None]*2
-    nn[0] = Layer(n=10,inn=9)
-    nn[1] = Layer(n=3,inn=10)
+    nn[0] = Layer(n=50,inn=9)
+    nn[1] = Layer(n=3,inn=50)
+    # nn[2] = Layer(n=3, inn=10)
     for l in nn:
         for i in range(l.num_nodes):
             l.nodes[i] = Node(l.num_ins)
@@ -163,9 +165,9 @@ def improved_agent(img, alpha, epoch):
         r = randint(1, len(l_gr)-2)
         c = randint(1, len(l_gr[0])-2)
         # 1x9 vector
-        x = l_gr[np.ix_(list(range(r-1,r+2)),list(range(c-1,c+2)))]
-        x = x.flatten()
-        x = np.multiply(x,float(1/255))
+        x_mat = l_gr[np.ix_(list(range(r-1,r+2)),list(range(c-1,c+2)))]
+        x_flat = x_mat.flatten()
+        x = np.multiply(x_flat,float(1/255))
         #x = np.append(x,1)
         #print("x: " + str(x))
         # actual color
@@ -173,16 +175,19 @@ def improved_agent(img, alpha, epoch):
         # get the predicted color from forward propogation
         pred_color = fwd_prop(nn, x)
         diff = (pred_color-actual)
-        diff = (np.dot(diff,diff))/3.0
+        err = 0.0
+        for i in range(len(diff)):
+            err += (diff[i]**2)
+        err /= 3.0
         # update the weight vectors
         back_prop(nn, actual)
         update_w(nn, x, alpha)
         if e % 1000 == 0:
-            print("epoch " + str(e))
+            print("step " + str(e))
             print("x: " + str(x))
             print("actual: " + str(actual) )
             print("pred: " + str(pred_color) )
-            print("error: " + str(diff))
+            print("error: " + str(err))
 
 
     # TESTING - use the model to recolor image
@@ -193,9 +198,9 @@ def improved_agent(img, alpha, epoch):
             if r == 0 or r == len(r_gr)-1 or c == 0 or c == len(r_gr[0])-1:
                 new_img[r].append([0,0,0])
                 continue
-            x = r_gr[np.ix_(list(range(r-1,r+2)),list(range(c-1,c+2)))]
-            x = x.flatten()
-            x = np.multiply(x,float(1/255))
+            x_mat = r_gr[np.ix_(list(range(r-1,r+2)),list(range(c-1,c+2)))]
+            x_flat = x_mat.flatten()
+            x = np.multiply(x_flat,float(1/255))
             #x = np.append(x,1)
             # compute the predicted color for l_img[r][c]
             color = fwd_prop(nn, x)